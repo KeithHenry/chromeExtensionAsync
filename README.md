0ebcc40ac199f004bff0e3e365aa5e9a1140bbda
Keith Henry <Keith.Henry@evolutionjobs.co.uk>
2017-03-10 13:56:29 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # chromeExtensionAsync
 Promise wrapper for the Chrome extension API so that it can be used with async/await rather than callbacks
+
+The [Extension API](https://developer.chrome.com/extensions) provided by Chrome uses callbacks. 
+However, Chrome now supports `async` and `await` keywords.
+
+This library wraps Chrome extension API callback methods in promises, so that they can be called with `async` and `await`.
+
+For instance, to get the current active tab:
+
+```javascript
+function startDoSomething(callback) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        var activeTab = tabs[0];
+
+        // Do stuff with activeTab...
+
+        callback(activeTab);
+    });
+}
+```
+
+This is replaced with:
+
+```javascript
+async function doSomething() {
+    const tabs = await chrome.tabsAsync.query({ active: true, currentWindow: true });
+    const activeTab = tabs[0];
+
+    // Do stuff with activeTab...
+
+    return activeTab;
+}
+```
+
+Chrome supports this new ES2017 syntax, so in extensions we can take full advantage of it.
---
+++ chrome-extension-async.js
@@ -0,0 +1,49 @@
+/** Wrap an API that uses callbacks with Promises
+ * This expects the pattern function withCallback(arg1, arg2, ... argN, callback) */
+class AsyncCallbackWrapper {
+    
+    /** Create a promise API from a callback one.
+     * @param callbackApi {object} The callback API to 'promisify'
+     * @param callbackFunctions {string[]} The names of functions with the pattern: withCallback(arg1, arg2, ... argN, callback) */
+    constructor(callbackApi, ...callbackFunctions) {
+        this.promisifyKnownCallbacks(callbackApi, new Set(callbackFunctions));
+    }
+
+    /** Set all the own-properties to this, wrap any known to be callbacks in a Promise.
+     * @param o {object} The instance to copy members from.
+     * @param known {Set} The names of any member functions that should be wrapped in promises. */
+    promisifyKnownCallbacks(o, known) {
+        for (let p in o) {
+            if(!o.hasOwnProperty(p))
+                continue; // Don't bother with .toString() etc
+
+            const m = o[p];
+            if (typeof m === 'function' && 
+                known.has(p)) {
+                // Wrap the source callback function in a promise so that we can call it with await
+                this[p] = this.promisify(m);
+            }
+            else {
+                // Not a known callback, just reference direct
+                this[p] = m;
+            }
+        }
+    }
+
+    /** Wrap a function with a callback with a Promise.
+     * @param f The function to wrap, should be pattern: withCallback(arg1, arg2, ... argN, callback).
+     * @returns Promise that resolves when the callback fires. */
+    promisify(f) {
+        return (...args) =>
+            new Promise((resolve, reject) => {
+                try {
+                    f(...args, resolve);
+                }
+                catch (err) {
+                    reject(err);
+                }
+            });
+    }
+}
+
+chrome.tabsAsync = new AsyncCallbackWrapper(chrome.tabs, 'get','getCurrent','sendRequest','sendMessage','getSelected','getAllInWindow','create','duplicate','query','highlight','update','move','reload','remove','detectLanguage','captureVisibleTab','executeScript','insertCSS','setZoom','getZoom','setZoomSettings','getZoomSettings','discard');
