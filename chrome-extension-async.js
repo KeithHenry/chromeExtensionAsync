fe04a3edbd8bb2fc362c1bc77bea9dc9e157cc45
KeithHenry <Keith.Henry@evolutionjobs.co.uk>
2017-03-13 09:02:42 +0000

    Added lots of missing API

--- chrome-extension-async.js
+++ chrome-extension-async.js
@@ -44,7 +44,8 @@
      * @param {object} callbackApi The callback API to 'promisify'
      * @param {string[]} callbackFunctions The names of functions with the pattern: withCallback(arg1, arg2, ... argN, callback) */
     function addAsyncWrappers(callbackApi, ...callbackFunctions) {
-        promisifyKnownCallbacks(callbackApi, new Set(callbackFunctions));
+        if (callbackApi)
+            promisifyKnownCallbacks(callbackApi, new Set(callbackFunctions));
     }
 
     // chrome.tabs https://developer.chrome.com/extensions/tabs
@@ -61,15 +62,94 @@
         'requestUpdateCheck', 'restartAfterDelay', 'sendMessage',
         'sendNativeMessage', 'getPlatformInfo', 'getPackageDirectoryEntry');
 
-    // StorageArea https://developer.chrome.com/extensions/storage#type-StorageArea
-    const knownInStorageArea = ['get', 'getBytesInUse', 'set', 'remove', 'clear'];
-    addAsyncWrappers(chrome.storage.sync, ...knownInStorageArea);
-    addAsyncWrappers(chrome.storage.local, ...knownInStorageArea);
-    addAsyncWrappers(chrome.storage.managed, ...knownInStorageArea);
+    // chrome.permissions https://developer.chrome.com/extensions/permissions
+    addAsyncWrappers(chrome.permissions, 'getAll', 'contains', 'request', 'remove');
 
     // chrome.identity https://developer.chrome.com/extensions/identity
     addAsyncWrappers(chrome.identity,
-        'getAuthToken', 'getProfileUserInfo', 'removeCachedAuthToken', 
+        'getAuthToken', 'getProfileUserInfo', 'removeCachedAuthToken',
         'launchWebAuthFlow', 'getRedirectURL');
 
+    // chrome.bookmarks https://developer.chrome.com/extensions/bookmarks
+    addAsyncWrappers(chrome.bookmarks,
+        'get', 'getChildren', 'getRecent', 'getTree', 'getSubTree',
+        'search', 'create', 'move', 'update', 'remove', 'removeTree');
+
+    // chrome.browserAction https://developer.chrome.com/extensions/browserAction
+    addAsyncWrappers(chrome.browserAction,
+        'getTitle', 'setIcon', 'getPopup', 'getBadgeText', 'getBadgeBackgroundColor');
+
+    // chrome.pageAction https://developer.chrome.com/extensions/pageAction
+    addAsyncWrappers(chrome.pageAction, 'getTitle', 'setIcon', 'getPopup');
+
+    // chrome.browsingData https://developer.chrome.com/extensions/browsingData
+    addAsyncWrappers(chrome.browsingData,
+        'settings', 'remove', 'removeAppcache', 'removeCache',
+        'removeCookies', 'removeDownloads', 'removeFileSystems',
+        'removeFormData', 'removeHistory', 'removeIndexedDB',
+        'removeLocalStorage', 'removePluginData', 'removePasswords',
+        'removeWebSQL');
+
+    // chrome.downloads https://developer.chrome.com/extensions/downloads
+    addAsyncWrappers(chrome.downloads,
+        'download', 'search', 'pause', 'resume', 'cancel',
+        'getFileIcon', 'erase', 'removeFile', 'acceptDanger');
+
+    // chrome.history https://developer.chrome.com/extensions/history
+    addAsyncWrappers(chrome.history, 'search', 'getVisits', 'addUrl', 'deleteUrl', 'deleteRange', 'deleteAll');
+
+    // chrome.alarms https://developer.chrome.com/extensions/alarms
+    addAsyncWrappers(chrome.alarms, 'get', 'getAll', 'clear', 'clearAll');
+
+    // chrome.i18n https://developer.chrome.com/extensions/i18n
+    addAsyncWrappers(chrome.i18n, 'getAcceptLanguages', 'detectLanguage');
+
+    // chrome.commands https://developer.chrome.com/extensions/commands#method-getAll
+    addAsyncWrappers(chrome.commands, 'getAll');
+
+    // chrome.contextMenus https://developer.chrome.com/extensions/contextMenus
+    addAsyncWrappers(chrome.contextMenus, 'create', 'update', 'remove', 'removeAll');
+
+    // chrome.extension https://developer.chrome.com/extensions/extension (mostly deprecated in favour of runtime)
+    addAsyncWrappers(chrome.extension, 'isAllowedIncognitoAccess', 'isAllowedFileSchemeAccess');
+
+    // chrome.cookies https://developer.chrome.com/extensions/cookies
+    addAsyncWrappers(chrome.cookies, 'get', 'getAll', 'set', 'remove', 'getAllCookieStores');
+
+    // chrome.windows https://developer.chrome.com/extensions/windows
+    addAsyncWrappers(chrome.windows, 'get', 'getCurrent', 'getLastFocused', 'getAll', 'create', 'update', 'remove');
+
+    // chrome.debugger https://developer.chrome.com/extensions/debugger
+    addAsyncWrappers(chrome.debugger, 'attach', 'detach', 'sendCommand', 'getTargets')
+
+    // chrome.desktopCapture https://developer.chrome.com/extensions/desktopCapture
+    addAsyncWrappers(chrome.desktopCapture, 'chooseDesktopMedia');
+
+    if (chrome.storage) {
+        // StorageArea https://developer.chrome.com/extensions/storage#type-StorageArea
+        // Todo: this should extend StorageArea.prototype instead
+        const knownInStorageArea = ['get', 'getBytesInUse', 'set', 'remove', 'clear'];
+        addAsyncWrappers(chrome.storage.sync, ...knownInStorageArea);
+        addAsyncWrappers(chrome.storage.local, ...knownInStorageArea);
+        addAsyncWrappers(chrome.storage.managed, ...knownInStorageArea);
+    }
+
+    if (chrome.contentSettings) {
+        // ContentSetting https://developer.chrome.com/extensions/contentSettings#type-ContentSetting
+        // Todo: this should extend ContentSetting.prototype instead
+        const knownInContentSetting = ['clear', 'get', 'set', 'getResourceIdentifiers'];
+        addAsyncWrappers(chrome.contentSettings.cookies, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.images, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.javascript, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.location, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.plugins, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.popups, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.notifications, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.fullscreen, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.mouselock, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.microphone, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.camera, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.unsandboxedPlugins, ...knownInContentSetting);
+        addAsyncWrappers(chrome.contentSettings.automaticDownloads, ...knownInContentSetting);
+    }
 })();
